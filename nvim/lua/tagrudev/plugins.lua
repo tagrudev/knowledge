7bd330194a0afb244244749cb54a40a841151a67
Todor Grudev <tagrudev@gmail.com>
2022-08-14 22:37:45 +0300

    Add toggleterm to neovim

--- nvim/lua/tagrudev/plugins.lua
+++ nvim/lua/tagrudev/plugins.lua
@@ -88,6 +88,9 @@ return packer.startup(function(use)
   -- Null LS
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
+  -- Toggleterm
+  use {"akinsho/toggleterm.nvim", tag = 'v2.*' }
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
---
+++ nvim/lua/tagrudev/toggleterm.lua
@@ -0,0 +1,55 @@
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+  return
+end
+
+toggleterm.setup({
+  size = 20,
+  open_mapping = [[<C-Bs>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+})
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-w>', [[<C-\><C-n><C-W>h]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+local Terminal = require("toggleterm.terminal").Terminal
+local node = Terminal:new({ cmd = "node", hidden = true })
+
+function _NODE_TOGGLE()
+  node:toggle()
+end
+
+local htop = Terminal:new({ cmd = "htop", hidden = true })
+
+function _HTOP_TOGGLE()
+  htop:toggle()
+end
+
+local irb = Terminal:new({ cmd = "irb", hidden = true })
+
+function _IRB_TOGGLE()
+  irb:toggle()
+end
